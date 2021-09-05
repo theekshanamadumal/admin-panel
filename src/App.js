dd84db9fc2106872f5003c27d75526ae0cb2bec4
theekshanamadumal <66960247+theekshanamadumal@users.noreply.github.com>
2021-09-05 18:54:27 +0530

    initialize

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Sidebar from "./components/sidebar/Sidebar";
+import Topbar from "./components/topbar/Topbar";
+import'./app.css'
+import Home from "./components/pages/Home/Home";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Topbar/>
+      <div className='contain'>
+        <Sidebar/>
+        <Home/>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
